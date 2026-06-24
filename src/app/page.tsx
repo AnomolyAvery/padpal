520537debc27c603aecc09b91962f835129a2496
AnomolyAvery <ashawsolutions@gmail.com>
2026-06-24 19:25:05 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <main>
+      <h1>Hi! Welcome to PadPal...</h1>
+    </main>
+  );
+}
---
+++ src/server/better-auth/config.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { organization } from "better-auth/plugins";
+
+import { db } from "@/server/db";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "pg" or "mysql"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [organization()],
+});
+
+export type Session = typeof auth.$Infer.Session;
